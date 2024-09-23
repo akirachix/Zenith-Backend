69e60550afa51fd081c0f4ac8dd3d6f39036b9cc
Linet Ndau <mkandoelinet@gmail.com>
2024-09-23 10:55:14 +0300

    changes

--- aquasens/settings.py
+++ aquasens/settings.py
@@ -101,15 +101,16 @@ WSGI_APPLICATION = "aquasens.wsgi.application"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#databases
 
 import os
+import dj_database_url
 
-
-DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
+DATABASES = {"default": dj_database_url.config(default=os.getenv("DATABASE_URL"))}
+if not os.getenv("DATABASE_URL"):
+    DATABASES = {
+        "default": {
+            "ENGINE": "django.db.backends.sqlite3",
+            "NAME": BASE_DIR / "db.sqlite3",
+        }
     }
-}
-
 
 # Password validation
 # https://docs.djangoproject.com/en/5.1/ref/settings/#auth-password-validators
